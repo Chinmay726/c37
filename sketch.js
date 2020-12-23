2d057f35435aee1e23a33676926781f67e26957d
Chinmay726 <70143561+Chinmay726@users.noreply.github.com>
2020-12-23 16:50:06 +0530

    c37

--- sketch.js
+++ sketch.js
@@ -9,7 +9,16 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+var car1i, car2i, car2i, car4i, groundi;
+var track;
+function preload(){
+car1i = loadImage('../images/car1.png');
+car2i = loadImage('../images/car2.png');
+car3i = loadImage('../images/car3.png');
+car4i = loadImage('../images/car4.png');
+groundi = loadImage('../images/track.png');
+track = loadImage('../images/track.jpg');
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +36,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
